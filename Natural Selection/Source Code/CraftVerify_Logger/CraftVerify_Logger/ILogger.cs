1e84e3a1921fac2c605f824b9404735e1e06a121
Parth <officialparththanki@gmail.com>
2024-01-19 02:42:55 -0800

    New Logger Library

---
+++ Natural Selection/Source Code/CraftVerify_Logger/CraftVerify_Logger/ILogger.cs
@@ -0,0 +1,7 @@
+﻿public interface ILogger
+{
+    Task LogAsync(LogEntry entry);
+    Task ArchiveLogsAsync();
+
+    Task WaitForQueueToEmptyAsync(CancellationToken cancellationToken = default);
+}
---
+++ Natural Selection/Source Code/CraftVerify_Logger/CraftVerify_Logger/ServiceConfigurator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+class ServiceConfigurator
+{
+    static void Main(string[] args)
+    {
+        // Setup DI
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<ILoggingDataAccess, LoggingDataAccess>(serviceProvider =>
+                new LoggingDataAccess("Server=localhost;Database=master;User Id=Parth;Password=1762;TrustServerCertificate=true"))
+
+            .AddSingleton<ILoggingDataAccess, LoggingDataAccess>()
+            .AddSingleton<ILogger, Logger>()
+            .BuildServiceProvider();
+
+        // Retrieve the logger instance
+        var logger = serviceProvider.GetService<ILogger>();
+    }
+}
